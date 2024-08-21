46c952f02e866ff6aa102af7974589a50fbab28c
Rachit-M24 <rachitm2022@gmail.com>
2024-08-21 22:00:49 +0530

    Add a updated file of oop.py

--- 21-08-24/oop.py
+++ 21-08-24/oop.py
@@ -11,18 +11,31 @@
 # print(my_car.brand);
 # print(my_car.model);
 
-#Second: Inheritance.
-class Info:
-  def __init__(self, name, age,city):
-    self.name = name;
-    self.age = age;
-    self.city = city;
+# This is code for OOPS concepts. Time:9:30P.M
 
-  def Sentence(self):
-      return f"{self.name} is {int(self.age)} years old and is from {self.city}"
-    
-Person1 = Info("person1",19,"jabalpur");
-print(Person1.Sentence())
+#Second: Inheritance.
+
+# class Info:
+#   def __init__(self, name, age,city):
+#     self.name = name;
+#     self.age = age;
+#     self.city = city;
+
+#   def Sentence(self):
+#       return f"{self.name} is {int(self.age)} years old and is from {self.city}"
+
+# class Employee(Info):
+#   def __init__(self,name, age,current_city,job_role):
+#        self.current_city = current_city;
+#        self.job_role = job_role;
+#        super().__init__(name, age , city = current_city)
+
+# Person1 = Info("Raju",19,"jabalpur");
+# print(Person1.Sentence())
+
+# Employee1 = Employee("Raju",22,"lucknow","SDE")
+# print(f"{Employee1.Sentence()} working as {Employee1.job_role}")
+
 
 
 
